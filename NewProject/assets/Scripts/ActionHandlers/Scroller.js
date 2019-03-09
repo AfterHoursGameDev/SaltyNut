6732587808102758aa4b88d8cfb29ac3c159de72
Lysle Shields <lysle@lesink.com>
2019-03-09 16:19:47 -0600

    Fixed scrolling background.

--- NewProject/assets/Scripts/ActionHandlers/Scroller.js
+++ NewProject/assets/Scripts/ActionHandlers/Scroller.js
@@ -7,16 +7,30 @@ cc.Class({
         //-- X轴边缘
         resetX: 0
     },
+	resetPos: null,
+	
+	onLoad()
+	{
+		this.resetPos = this.node.getPosition();
+	},
 
     update (dt) {
-        if (D.game.state !== D.GameManager.State.Run) {
-            return;
-        }
-        var x = this.node.x;
+//        if (D.game.state !== D.GameManager.State.Run) {
+//            return;
+//        }
+		var pos = this.node.getPosition();
+        var x = pos.x;
+console.log("x=" + x + ", dt=" + dt + ", speed="+this.speed);
         x += this.speed * dt;
+console.log("newx = " + x);		
         if (x <= this.resetX) {
-            x -= this.resetX;
+			pos = this.resetPos;
         }
-        this.node.x = x;
+		else
+		{
+			pos.x = x;
+		}
+console.log("pos = " + pos);
+		this.node.setPosition(pos);
     }
 });
